2bbb407c8d075576228b1569c7af07935af276c4
Dustin Aldana <71577541+D-Aldana@users.noreply.github.com>
2025-02-13 21:26:27 -0800

    Add message

--- react-app/src/components/Valentines/index.js
+++ react-app/src/components/Valentines/index.js
@@ -23,44 +23,46 @@ const Contact = () => {
 
   return (
     <div className={styles.container}>
-      <svg viewBox="0 0 400 150" className={styles.svgText}>
-        <path id="curve" d="M10,100 Q200,-50 390,100" fill="transparent" />
-        <text>
-          <textPath
-            href="#curve"
-            startOffset="50%"
-            textAnchor="middle"
-            spacing="auto"
-          >
-            Will you be my valentine?
-          </textPath>
-        </text>
-      </svg>
-
       {!clickedYes && (
-        <div className={styles.buttons}>
-          <button
-            className={styles.noBtn}
-            onClick={handleNoClick}
-            style={{
-              top: noButtonPosition.top,
-              left: noButtonPosition.left,
-              position: initial ? '' : 'absolute',
-              transform: initial ? '' : 'translate(-50%, -50%)',
-            }}
-          >
-            No
-          </button>
-          <button
-            className={styles.yesBtn}
-            onClick={() => setClickedYes(true)}
-            style={{
-              transform: `scale(${yesButtonSize})`,
-            }}
-          >
-            Yes
-          </button>
-        </div>
+        <>
+          <svg viewBox="0 0 400 150" className={styles.svgText}>
+            <path id="curve" d="M10,100 Q200,-50 390,100" fill="transparent" />
+            <text>
+              <textPath
+                href="#curve"
+                startOffset="50%"
+                textAnchor="middle"
+                spacing="auto"
+              >
+                Will you be my valentine?
+              </textPath>
+            </text>
+          </svg>
+
+          <div className={styles.buttons}>
+            <button
+              className={styles.noBtn}
+              onClick={handleNoClick}
+              style={{
+                top: noButtonPosition.top,
+                left: noButtonPosition.left,
+                position: initial ? '' : 'absolute',
+                transform: initial ? '' : 'translate(-50%, -50%)',
+              }}
+            >
+              No
+            </button>
+            <button
+              className={styles.yesBtn}
+              onClick={() => setClickedYes(true)}
+              style={{
+                transform: `scale(${yesButtonSize})`,
+              }}
+            >
+              Yes
+            </button>
+          </div>
+        </>
       )}
 
       {/* Render hearts randomly around the page */}
@@ -81,10 +83,46 @@ const Contact = () => {
       </div>
 
       {clickedYes && (
-        <div className={styles.yesMessage}>
-          <h1 className={styles.yay}>Yay! I'm so happy you said yes!</h1>
-          <img src="/zesty.gif" autoPlay loop muted alt="Video here" />
-        </div>
+        <>
+          <div className={styles.yesMessage}>
+            <h1 className={styles.yay}>Yay! I'm so happy you said yes!</h1>
+            <img src="/zesty.gif" autoPlay loop muted alt="Video here" />
+          </div>
+
+          <div
+            className={styles.message}
+            style={{
+              background: "url('/parchment.jpg') no-repeat center center",
+            }}
+          >
+            <span>Dear Trisha, </span>
+            <span>
+              As of today, we've had 10 months of McFlurries, shared 45 weeks of
+              Chicko Chicken, 320 days of calling you babe until you got used to
+              it, 7680 hours of smelling each other's farts, 460,800 minutes
+              snoring on the phone, and 27,648,000 seconds of being interrupted
+              by phone calls from your parents.
+            </span>
+            <br />
+            <span>
+              When you walked up 45 minutes lates on the day we met, and even on
+              this day 1 year ago on the ice, I could've never known that this
+              much time later, you would mean so much to me. I know it's not
+              easy being far apart like this, but one day we'll no longer have
+              to say goodbye. Instead, it'll be see you at home. We have our ups
+              and our downs, but I'd stay awake through 1000 more bad nights if
+              it means I get to wake up next to you every morning.
+            </span>
+            <br />
+            <span>
+              Each moment passed is another moment closer to our next moment
+              together. Thank you for loving me. I love you more than you will
+              ever know. Happy Valentine's Day, my baby.
+            </span>
+            <br />
+            <span>Love, Dokie</span>
+          </div>
+        </>
       )}
     </div>
   )
